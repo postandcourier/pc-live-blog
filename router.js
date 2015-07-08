971392fc19350dbf0382074f69362afbb7629955
J Emory Parker <eparker@postandcourier.com>
2015-07-08 12:04:55 -0400

    lastest updates

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-Router.route('/', function () {
+Router.route('/live-blog', function () {
   this.render('main');
 });
 
-Router.route('/admin', function () {
+Router.route('/live-blog/admin', function () {
   this.render('admin');
 });
